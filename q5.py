446de23989a279bdb428768be5f1f1fa6e50d73f
Jossy Mesfin Yoseph <jmesfin@ju2.edu.et>
2026-03-09 21:44:55 -0400

    initial commit for the question -5

---
+++ q5.py
@@ -0,0 +1,6 @@
+# 5)	Create a basic quiz game that:
+# •	Contains a list of 5–10 questions stored in a dictionary (or list of dictionaries [{}, {}] ).
+# •	Ask the user each question and records their answers.
+# •	At the end, displays:
+# o	The user's score (e.g., 7/10)
+# o	Correct answers for any questions they got wrong
