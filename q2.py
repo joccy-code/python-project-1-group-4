3ffda25fcccaf47bbc28df21e3d241207a287ada
fertunagebe-dev <fertunagebe@gmail.com>
2026-03-09 19:51:55 -0700

    this my contribution

---
+++ q2.py
@@ -0,0 +1,6 @@
+# 2)	Write a program that:
+# •	Has a predefined dictionary of groceries with prices.
+# •	Lets the user "add" items by typing their names.
+# •	For each valid item, asks for the quantity.
+# •	Keeps adding to the cart until the user types "checkout".
+# •	Displays a final bill: each item, quantity, subtotal, and total.
